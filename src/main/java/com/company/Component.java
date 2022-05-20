3dc1705b4fdc18e5eadcc59e93136ff03b4310ed
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2022-05-20 02:48:20 +0200

    smalledit

--- src/main/java/com/company/Component.java
+++ src/main/java/com/company/Component.java
@@ -1,7 +1,7 @@
 package com.company;
 /**
  * Class component describes each component with all its parameters that it needs
- * and is what the topology consists of
+ * and is what the topology consists of.
  * @author Abdelrahman Noaman
  * @version 1.0.0 May 17, 2022
  */
@@ -12,10 +12,10 @@ public class Component {
     private Device componentDevice;
 
     /**
-     * Non Default Component Constructor to set the data member of the component
-     * @param componentType - Component type
-     * @param componentId - Unique Component ID in each topology
-     * @param ComponentDevice - device object to set it's default, min and max
+     * Non Default Component Constructor to set the data member of the component.
+     * @param componentType - Component type.
+     * @param componentId - Unique Component ID in each topology.
+     * @param ComponentDevice - device object to set it's default, min and max.
      */
     public Component(String componentType, String componentId,Device ComponentDevice) {
         setComponentType(componentType);
@@ -23,14 +23,14 @@ public class Component {
         setComponentDevice((ComponentDevice));
     }
     /**
-     * Default Component Constructor
+     * Default Component Constructor.
      */
     public Component() {
         setComponentType("");
         setComponentId("");
     }
     /**
-     * Print component function which is used to print the details of the netlist of the device
+     * Print component function which is used to print the details of the netlist of the device.
      */
     public void printComponent(){
         System.out.println("Component id ="+ComponentId);
@@ -38,43 +38,43 @@ public class Component {
         componentDevice.PrintDevice();
     }
     /**
-     * Get the component type
-     * @return type
+     * Get the component type.
+     * @return type.
      */
     public String getComponentType() {
         return ComponentType;
     }
     /**
-     * Set component type
-     * @param componentType - The type of the component
+     * Set component type.
+     * @param componentType - The type of the component.
      */
     public void setComponentType(String componentType) {
         ComponentType = componentType;
     }
     /**
-     * Get the component ID
-     * @return ComponentID
+     * Get the component ID.
+     * @return ComponentID.
      */
     public String getComponentId() {
         return ComponentId;
     }
     /**
-     * Set component ID
-     * @param componentId - The ID of the component
+     * Set component ID.
+     * @param componentId - The ID of the component.
      */
     public void setComponentId(String componentId) {
         ComponentId = componentId;
     }
     /**
-     * Get the Device "ComponentDevice"
-     * @return Device of the component
+     * Get the Device "ComponentDevice".
+     * @return Device of the component.
      */
     public Device getComponentDevice() {
         return componentDevice;
     }
     /**
      * Set Device
-     * @param componentDevice - the device of the component
+     * @param componentDevice - the device of the component.
      */
 
     public void setComponentDevice(Device componentDevice) {
